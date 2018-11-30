ec1adcfd14ab2f3732575edebd12c7d65e9a5754
Nicolas Boucher <nboucher@ulb.ac.be>
2018-11-30 15:11:39 +0100

    18 - load world, world1.txt

--- src/main/worlds/World.java
+++ src/main/worlds/World.java
@@ -3,11 +3,13 @@ package main.worlds;
 import java.awt.Graphics;
 
 import main.tiles.Tile;
+import main.utils.Utils;
 
 public class World {
 	
 	// unit = number of tiles
 	private int width, height;
+	private int spawnX, spawnY;
 	private int[][] tiles;
 	
 	public World(String path){
@@ -34,7 +36,19 @@ public class World {
 	}
 	
 	private void loadWorld(String path){
+		String file = Utils.loadFileAsString(path);
+		String[] tokens = file.split("\\s+");
+		width = Utils.parseInt(tokens[0]);
+		height = Utils.parseInt(tokens[1]);
+		spawnX = Utils.parseInt(tokens[2]);
+		spawnY = Utils.parseInt(tokens[3]);
 		
+		tiles = new int[width][height];
+		for(int y = 0; y < height; y++){
+			for(int x = 0; x < width; x++){
+				tiles[x][y] = Utils.parseInt(tokens[(x + y*width)+4]);
+			}
+		}
 	}
 	
 }
